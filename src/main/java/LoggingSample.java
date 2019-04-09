a05d62841c47de61288bedeaa49ab74b056c6a2f
john <joje@data-alliance.com>
2019-04-09 15:41:08 +0900

    Initial commit

---
+++ src/main/java/LoggingSample.java
@@ -0,0 +1,10 @@
+public class LoggingSample {
+
+    public void someMethod() {
+        System.out.println("Hello log!");
+    }
+
+    public static void main(String[] args) {
+        new LoggingSample().someMethod();
+    }
+}
